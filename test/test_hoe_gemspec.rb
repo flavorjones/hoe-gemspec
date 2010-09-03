8dfc5b7014e3827d5adbfe377614cfa195104981
Mike Dalessio <mike@csa.net>
2010-09-03 12:29:52 -0400

    removing unused variable

--- test/test_hoe_gemspec.rb
+++ test/test_hoe_gemspec.rb
@@ -3,7 +3,6 @@ require "fileutils"
 
 class TestHoeBundler < Test::Unit::TestCase
   def test_output
-    gemfile = nil
     Dir.chdir(File.join(File.dirname(__FILE__), "fixture_project")) do
       FileUtils.rm_f "fixture_project.gemspec"
       system "rake gem:spec"
